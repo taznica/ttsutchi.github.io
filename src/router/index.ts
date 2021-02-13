1c7e70cd503cc64f9276cdec40301ac3829507af
taznica <tailegog@gmail.com>
2021-02-13 21:42:01 +0900

    change: initialize project with Vue 3 + Typescript

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,7 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
-
-  const routes = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
@@ -20,9 +17,8 @@ Vue.use(VueRouter)
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // publicPath: '/',
-    // outputDir: 'docs',
-    filenameHashing: false,
-    productionSourceMap: false,
-}
