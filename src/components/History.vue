d38987e7278f6f3844e23294ca9786d4102b2400
taznica <tailegog@gmail.com>
2021-02-19 16:10:32 +0900

    update: remove padding in History.vue

--- src/components/History.vue
+++ src/components/History.vue
@@ -9,7 +9,7 @@
                 <h3>{{ event.title }}</h3>
             </v-stepper-step>
             <v-stepper-content :step="1">
-                <h4 class="mb-2 font-weight-medium" id="description"><span v-html="event.description" /></h4>
+                <h4 class="font-weight-medium" id="description"><span v-html="event.description" /></h4>
 <!--                <v-btn dark depressed color="amber" v-if="event.hasWork">詳細↓</v-btn>-->
             </v-stepper-content>
         </v-stepper>
