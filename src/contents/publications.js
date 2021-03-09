421243d4240a1680db7965247f2a26278f36bcba
taznica <tailegog@gmail.com>
2021-03-09 17:02:23 +0900

    update: add info of ShearSheet(ipsj) in publications.js, and change full-width comma and period to half-width

--- src/contents/publications.js
+++ src/contents/publications.js
@@ -1,9 +1,9 @@
 export default [
-    "藤田和之，Mengting Huang，高嶋和毅，<u>土田太一</u>，真鍋宏幸，北村喜文．ShearSheet: 静電容量タッチパネルに装着可能な透明シートを用いた剪断入力インタフェース．情報処理学会論文誌，to appear.",
+    "藤田和之, Mengting Huang, 高嶋和毅, <u>土田太一</u>, 真鍋宏幸, 北村喜文. ShearSheet: 静電容量タッチパネルに装着可能な透明シートを用いた剪断入力インタフェース. 情報処理学会論文誌, Vol. 62, No.2, pp. 641-653, 2021年2月. [<a href='http://doi.org/10.20729/00209337'>DOI</a>]",
 
-    "<u>土田太一</u>，藤田和之，中原和洋，山田茂雄，高嶋和毅，北村喜文．机上へのプロジェクションによる紙面テキストハイライト手法．インタラクション2020論文集，pp. 909-911, 2020年3月．[<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-30.html'>Website</a>] [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-30.pdf'>PDF</a>] <b><i>プレミアム発表</i></b>.",
+    "<u>土田太一</u>, 藤田和之, 中原和洋, 山田茂雄, 高嶋和毅, 北村喜文. 机上へのプロジェクションによる紙面テキストハイライト手法. インタラクション2020論文集, pp. 909-911, 2020年3月. [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-30.html'>Website</a>] [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-30.pdf'>PDF</a>] <b><i>プレミアム発表</i></b>.",
 
-    "中原和洋，藤田和之，<u>土田太一</u>，高嶋和毅，北村喜文．バーチャルな影による奥行き錯覚を用いた付箋紙の移動操作と類似度可視化インタフェース．インタラクション2020論文集，pp. 848-851, 2020年3月. [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-12.html'>Website</a>] [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-12.pdf'>PDF</a>] <b><i>プレミアム発表</i></b>.",
+    "中原和洋, 藤田和之, <u>土田太一</u>, 高嶋和毅, 北村喜文. バーチャルな影による奥行き錯覚を用いた付箋紙の移動操作と類似度可視化インタフェース. インタラクション2020論文集, pp. 848-851, 2020年3月. [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-12.html'>Website</a>] [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-12.pdf'>PDF</a>] <b><i>プレミアム発表</i></b>.",
 
     "Mengting Huang, Kazuyuki Fujita, Kazuki Takashima, <u>Taichi Tsuchida</u>, Hiroyuki Manabe, Yoshifumi Kitamura. ShearSheet: Low-Cost Shear Force Input with Elastic Feedback for Augmenting Touch Interaction. Proc. of the 2019 ACM Interactive Surfaces and Spaces (ISS '19), pp. 77-87, Nov. 2019. [<a href='https://doi.org/10.1145/3343055.3359717'>DOI</a>] <b><i>Best Demo Award</i></b>.",
 
