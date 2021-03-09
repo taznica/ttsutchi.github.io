5db967810a3827b6ddd5e96076c9bf732a82025b
taznica <tailegog@gmail.com>
2021-03-09 16:51:12 +0900

    update: style of publications in publications.js

--- src/contents/publications.js
+++ src/contents/publications.js
@@ -1,22 +1,13 @@
 export default [
     "藤田和之，Mengting Huang，高嶋和毅，<u>土田太一</u>，真鍋宏幸，北村喜文．ShearSheet: 静電容量タッチパネルに装着可能な透明シートを用いた剪断入力インタフェース．情報処理学会論文誌，to appear.",
 
-    "<u>土田太一</u>，藤田和之，中原和洋，山田茂雄，高嶋和毅，北村喜文．机上へのプロジェクションによる紙面テキストハイライト手法．インタラクション2020論文集，pp. 909-911, 2020年3月．<b><i>プレミアム発表</i></b>. <br>" +
-    "<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-30.html'>https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-30.html</a> <br>" +
-    "<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-30.pdf'>https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-30.pdf</a>",
+    "<u>土田太一</u>，藤田和之，中原和洋，山田茂雄，高嶋和毅，北村喜文．机上へのプロジェクションによる紙面テキストハイライト手法．インタラクション2020論文集，pp. 909-911, 2020年3月．[<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-30.html'>Website</a>] [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-30.pdf'>PDF</a>] <b><i>プレミアム発表</i></b>.",
 
-    "中原和洋，藤田和之，<u>土田太一</u>，高嶋和毅，北村喜文．バーチャルな影による奥行き錯覚を用いた付箋紙の移動操作と類似度可視化インタフェース．インタラクション2020論文集，pp. 848-851, 2020年3月．<b><i>プレミアム発表</i></b>. <br>" +
-    "<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-12.html'>https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-12.html</a> <br>" +
-    "<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-12.pdf'>https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-12.pdf</a>",
+    "中原和洋，藤田和之，<u>土田太一</u>，高嶋和毅，北村喜文．バーチャルな影による奥行き錯覚を用いた付箋紙の移動操作と類似度可視化インタフェース．インタラクション2020論文集，pp. 848-851, 2020年3月. [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/bib/3B-12.html'>Website</a>] [<a href='https://www.interaction-ipsj.org/proceedings/2020/data/pdf/3B-12.pdf'>PDF</a>] <b><i>プレミアム発表</i></b>.",
 
-    "Mengting Huang, Kazuyuki Fujita, Kazuki Takashima, <u>Taichi Tsuchida</u>, Hiroyuki Manabe, Yoshifumi Kitamura. ShearSheet: Low-Cost Shear Force Input with Elastic Feedback for Augmenting Touch Interaction. Proc. of the 2019 ACM Interactive Surfaces and Spaces (ISS '19), pp. 77-87, Nov. 2019. DOI:<a href='https://doi.org/10.1145/3343055.3359717'>https://doi.org/10.1145/3343055.3359717</a>. " +
-    "<b><i>Best Demo Award</i></b>.",
+    "Mengting Huang, Kazuyuki Fujita, Kazuki Takashima, <u>Taichi Tsuchida</u>, Hiroyuki Manabe, Yoshifumi Kitamura. ShearSheet: Low-Cost Shear Force Input with Elastic Feedback for Augmenting Touch Interaction. Proc. of the 2019 ACM Interactive Surfaces and Spaces (ISS '19), pp. 77-87, Nov. 2019. [<a href='https://doi.org/10.1145/3343055.3359717'>DOI</a>] <b><i>Best Demo Award</i></b>.",
 
-    "Shotaro Ichikawa, Aoi Suzuki, Isamu Endo, Yuhei Osaka, Pascal Chiu, <u>Taichi Tsuchida</u>, and Takayuki Tanno. Tabletop ARrietty. 第24回日本バーチャルリアリティ学会大会論文集, 3D-5, September 2019. <b><i>審査員特別賞</i></b>.<br>" +
-    "<a href='http://ivrc.net/archive/tabletop-arrietty2019/'>http://ivrc.net/archive/tabletop-arrietty2019/</a> <br>" +
-    "<a href='http://conference.vrsj.org/ac2019/program/common/doc/pdf/3D-05.pdf'>http://conference.vrsj.org/ac2019/program/common/doc/pdf/3D-05.pdf</a>",
+    "Shotaro Ichikawa, Aoi Suzuki, Isamu Endo, Yuhei Osaka, Pascal Chiu, <u>Taichi Tsuchida</u>, and Takayuki Tanno. Tabletop ARrietty. 第24回日本バーチャルリアリティ学会大会論文集, 3D-5, September 2019. [<a href='http://ivrc.net/archive/tabletop-arrietty2019/'>Website</a>] [<a href='http://conference.vrsj.org/ac2019/program/common/doc/pdf/3D-05.pdf'>PDF</a>] <b><i>審査員特別賞</i></b>.",
 
-    "市川 将太郎, 鈴木 蒼生, 遠藤 勇, 大坂 侑平, 邱 家輝, <u>土田 太一</u>, 丹野 隆幸, 泉 健太. 蠢刺青. 第24回日本バーチャルリアリティ学会大会論文集, 2C-8, 2019年9月. <b><i>予選大会進出</i></b>.<br>" +
-    "<a href='http://ivrc.net/archive/%E8%A0%A2%E5%88%BA%E9%9D%922019/'>http://ivrc.net/archive/%E8%A0%A2%E5%88%BA%E9%9D%922019/</a> <br>" +
-    "<a href='http://conference.vrsj.org/ac2019/program/common/doc/pdf/2C-08.pdf'>http://conference.vrsj.org/ac2019/program/common/doc/pdf/2C-08.pdf</a>",
+    "市川 将太郎, 鈴木 蒼生, 遠藤 勇, 大坂 侑平, 邱 家輝, <u>土田 太一</u>, 丹野 隆幸, 泉 健太. 蠢刺青. 第24回日本バーチャルリアリティ学会大会論文集, 2C-8, 2019年9月. [<a href='http://ivrc.net/archive/%E8%A0%A2%E5%88%BA%E9%9D%922019/'>Website</a>] [<a href='http://conference.vrsj.org/ac2019/program/common/doc/pdf/2C-08.pdf'>PDF</a>] <b><i>予選大会進出</i></b>.",
 ]
